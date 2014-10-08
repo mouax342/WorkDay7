45c7afba25f55921b62bd92234ff34df83fe957a
idsc3101 <idsc3101@gmail.com>
2014-10-08 17:11:30 -0500

    solved problems for workday 7

--- Millionaire/Millionaire/Form1.vb
+++ Millionaire/Millionaire/Form1.vb
@@ -7,16 +7,17 @@ Public Class Form1
         Dim amountSt As String = txtAmount.Text
         If Not IsNumeric(amountSt) Then
             MessageBox.Show("Please type the numbers!")
-            Return
+            Return 'tells the code to jump to the end
         End If
 
         ' Convert into a number 
         Dim balance As Double, numYears As Integer
         balance = CDbl(amountSt)
 
+        Dim r As Double = 0.06
         ' Compute the time it takes to reach the balance of a million
-        Do While balance < 1000000
-            balance += 0.06 * balance
+        Do Until balance >= 1000000
+            balance += r * balance
             numYears += 1
         Loop
 
--- PopulationYear/PopulationYear/Form1.vb
+++ PopulationYear/PopulationYear/Form1.vb
@@ -3,7 +3,17 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As Object, e As EventArgs) Handles btnPopulation.Click
-        MessageBox.Show("The answer: not implemented yet!")
+
+        Dim pop As Double = 7000.0 'pop of 7bln in year of 2014 & doubles every 50 years
+        Dim year As Integer = 2014
+
+        Do While (pop >= 6)
+            pop = pop / 2
+            year = year - 50
+
+        Loop
+
+        MessageBox.Show("The answer is " & year)
 
     End Sub
 End Class
